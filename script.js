48aac0628731751f279ea6676b2dfa5200940a6b
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-04-18 10:36:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function insert_Row() {
     let newRow = table.insertRow(0);
     let cell1 = newRow.insertCell(0);
     let cell2 = newRow.insertCell(1);
-    cell1.innerHTML = 'New Cell1';
-    cell2.innerHTML = 'New Cell2';
+    cell1.textContent = 'New Cell1';
+    cell2.innerContent = 'New Cell2';
   
 }
